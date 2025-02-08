1558625db94d5cc2a39fb8c8e922e6384c89e508
filip <filipej.f.1020@gmail.com>
2025-02-08 00:35:22 -0300

    frontend commit

---
+++ student-service/src/main/java/com/DTOs/CreateStudentDTO.java
@@ -0,0 +1,4 @@
+package com.DTOs;
+
+public record CreateStudentDTO(String name, String email, Integer age, String phone){
+}
---
+++ student-service/src/main/java/com/DTOs/UpdateStudentDTO.java
@@ -0,0 +1,4 @@
+package com.DTOs;
+
+public record UpdateStudentDTO (String name, String email, Integer age, String phone){
+}

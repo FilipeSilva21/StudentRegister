9b2fe09b2bd76cbbf8ef3b2ac8e60114ffc0f921
Filipe Silva <118104038+FilipeSilva21@users.noreply.github.com>
2025-02-11 06:38:35 -0300

    Delete student-web directory

--- student-web/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { HomeComponent } from './student/home/home.component';
-
-export const routes: Routes = [
-  {path: "student/home", component:HomeComponent},
-  {path: "student", redirectTo:"student/home", pathMatch:"full"},
-  {path: "", redirectTo:"student/home", pathMatch:"full"}
-];
-
